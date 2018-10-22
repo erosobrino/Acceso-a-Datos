deca988a0c1a87ab3420f8e1cdc474550d63201f
Ero Sobrino <ero.sobrino.dorado@gmail.com>
2018-10-22 14:00:02 +0200

    22/10/18

--- src/Ejer1/Ejer1.java
+++ src/Ejer1/Ejer1.java
@@ -1,5 +1,7 @@
 package Ejer1;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.HashMap;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -12,6 +14,10 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSSerializer;
 
 public class Ejer1 {
 
@@ -19,24 +25,115 @@ public class Ejer1 {
 		Ejer1 ejer = new Ejer1();
 		Document doc = ejer.creaArbol("D:/Ciclo/Acceso a datos/Tema2/src/peliculas.xml");
 
-//		ejer.getDato(doc,"titulo");
-//		ejer.getTituloNombreApellidoGenero(doc);
-//		ejer.arbolRecursivo(doc, doc.getChildNodes(), -1);
-//		ejer.peliculasNDirectores(doc, 2);
-//		ejer.cantidadGenerosDistintos(doc);
-
-		ejer.añadeAtributoaPelicula(doc, "Matrix", "puntuacion");
-		NodeList peliculas=doc.getElementsByTagName("pelicula");
-		for (int i = 0; i < peliculas.getLength(); i++) {
-			NamedNodeMap atributos=peliculas.item(i).getAttributes();
-			for (int j = 0; j < atributos.getLength(); j++) {
-				System.out.println(atributos.item(j).getNodeName());
+//		ejer.getDato(doc,"titulo");//1
+//		ejer.getDato(doc, "titulo");//2
+//		ejer.getTituloNombreApellidoGenero(doc);//3
+//		ejer.arbolRecursivo(doc, doc.getChildNodes(), -1);//4
+//		ejer.peliculasNDirectores(doc, 1);//5
+//		System.out.println("Hay "+ejer.cantidadGenerosDistintos(doc)+" generos distintos");//6
+		
+//		ejer.añadeEliminaAtributoaPelicula(doc, "Dune", "Edad", true);//7
+//		muestraTituloAtributos(doc);
+//		ejer.añadeEliminaAtributoaPelicula(doc, "Dune", "Edad", false);
+//		muestraTituloAtributos(doc);
+		
+//		doc = ejer.añadeDom(doc, "Depredador", "1987", "John", "Tiernan", "Accion", "en");//8
+//		muestraTituloAtributos(doc);
+//		try {
+//			ejer.guardarDom(doc, "D:\\Ciclo\\Acceso a datos\\Tema2\\src\\salida.xml");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		
+//		ejer.modificarDom(doc, "nombre", "Larry", "Lana");//9
+//		try {
+//			ejer.guardarDom(doc, "D:\\Ciclo\\Acceso a datos\\Tema2\\src\\salida.xml");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		
+		ejer.añadirDirector();
+	}
+	
+	public void añadirDirector() {
+		
+	}
+	
+	public void modificarDom(Document doc, String nodoModificar, String textoAntiguo, String textoNuevo) {
+		NodeList nombres=doc.getElementsByTagName(nodoModificar);
+		for (int i = 0; i < nombres.getLength(); i++) {
+			if (nombres.item(i).getFirstChild().getNodeValue().equals(textoAntiguo)) {
+				nombres.item(i).getFirstChild().setNodeValue(textoNuevo);
 			}
-		}//no funciona
-
+		}
 	}
 
-	private void añadeAtributoaPelicula(Document doc, String titulo, String atributo) {
+	public void guardarDom(Document doc, String tituloSalida) throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException, ClassCastException, FileNotFoundException {
+		DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+		DOMImplementationLS ls = (DOMImplementationLS) registry.getDOMImplementation("XML 3.0 LS 3.0");
+		LSOutput output = ls.createLSOutput();
+		output.setEncoding("UTF-8");
+		output.setByteStream(new FileOutputStream(tituloSalida));
+		LSSerializer serializer = ls.createLSSerializer();
+		serializer.setNewLine("\r\n");
+		serializer.getDomConfig().setParameter("format-pretty-print", true);
+		serializer.write(doc, output);
+	}
+
+	public Document añadeDom(Document doc, String titulo, String año, String nombre, String apellido, String genero,
+			String idioma) {
+		try {
+			Element nodoPelicula = doc.createElement("pelicula");
+			nodoPelicula.setAttribute("año", año);
+			nodoPelicula.setAttribute("genero", genero);
+			nodoPelicula.setAttribute("idioma", idioma);
+			nodoPelicula.appendChild(doc.createTextNode("\n"));
+
+			Element nodoTitulo = doc.createElement("titulo");
+			Text textNodoTitulo = doc.createTextNode(titulo);
+			nodoTitulo.appendChild(textNodoTitulo);
+			nodoPelicula.appendChild(nodoTitulo);
+			nodoPelicula.appendChild(doc.createTextNode("\n"));
+
+			Element nodoDirector = doc.createElement("director");
+			nodoDirector.appendChild(doc.createTextNode("\n"));
+
+			Element nodoNombre = doc.createElement("nombre");
+			Text textNodoNombre = doc.createTextNode(nombre);
+			nodoNombre.appendChild(textNodoNombre);
+			nodoDirector.appendChild(nodoNombre);
+			nodoDirector.appendChild(doc.createTextNode("\n"));
+
+			Element nodoApellido = doc.createElement("apellido");
+			Text textNodoApellido = doc.createTextNode(apellido);
+			nodoApellido.appendChild(textNodoApellido);
+			nodoDirector.appendChild(nodoApellido);
+			nodoDirector.appendChild(doc.createTextNode("\n"));
+
+			nodoPelicula.appendChild(nodoDirector);
+			Node raiz = doc.getFirstChild();
+			raiz.appendChild(nodoPelicula);
+			System.out.println(raiz.getNodeName());
+		} catch (DOMException e) {
+			System.out.println("Error al crear Dom" + e.getMessage());
+		}
+		return doc;
+	}
+
+	public static void muestraTituloAtributos(Document doc) {
+		NodeList peliculas = doc.getElementsByTagName("pelicula");
+		for (int i = 0; i < peliculas.getLength(); i++) {
+			System.out.println(peliculas.item(i).getChildNodes().item(1).getFirstChild().getNodeValue());
+			NamedNodeMap atributos = peliculas.item(i).getAttributes();
+			for (int j = 0; j < atributos.getLength(); j++) {
+				System.out.println("\t" + atributos.item(j).getNodeName());
+			}
+			System.out.println();
+		}
+	}
+
+	private void añadeEliminaAtributoaPelicula(Document doc, String titulo, String atributo, boolean añadir) {
 		NodeList peliculas = doc.getElementsByTagName("pelicula");
 		boolean existe = false;
 		for (int i = 0; i < peliculas.getLength(); i++) {
@@ -46,34 +143,38 @@ public class Ejer1 {
 					if (datos.item(k).getFirstChild().getNodeValue().equals(titulo)) {
 						if (peliculas.item(i).hasAttributes()) {
 							NamedNodeMap atributos = peliculas.item(i).getAttributes();
-							for (int j = 0; j < atributos.getLength(); j++) {
-								if (!existe) {
-									if (atributos.item(j).getNodeName().equals(atributo)) {
-										existe = true;
+							if (añadir) {
+								for (int j = 0; j < atributos.getLength(); j++) {
+									if (!existe) {
+										if (atributos.item(j).getNodeName().equals(atributo)) {
+											existe = true;
+										}
 									}
 								}
-							}
-							if (!existe) {
-								((Element) peliculas.item(i)).setAttribute("sddffg", "sddffg");
-								System.out.println("affvadf");
+								if (!existe) {
+									((Element) peliculas.item(i)).setAttribute(atributo, "");
+								}
+							} else {
+								((Element) peliculas.item(i)).removeAttribute(atributo);
 							}
 						} else {
-							((Element) peliculas.item(i)).setAttribute("sddffg", "sddffg");
-							System.out.println("affvadf");
+							if (añadir) {
+								((Element) peliculas.item(i)).setAttribute(atributo, "");
+							}
 						}
 					}
-				}//
+				}
 			}
 		}
 	}
 
-	public void cantidadGenerosDistintos(Document doc) {
+	public int cantidadGenerosDistintos(Document doc) {
 		HashMap<String, Integer> generos = new HashMap<>();
 		NodeList peliculas = doc.getElementsByTagName("pelicula");
 		for (int i = 0; i < peliculas.getLength(); i++) {
 			NamedNodeMap atributos = peliculas.item(i).getAttributes();
 			for (int j = 0; j < atributos.getLength(); j++) {
-				if (atributos.item(j).getNodeName() == "genero") {
+				if (atributos.item(j).getNodeName().equals("genero")) {
 //					System.out.println(atributos.item(j).getNodeValue());
 					if (generos.containsKey(atributos.item(j).getNodeValue())) {
 						generos.put(atributos.item(j).getNodeValue(),
@@ -87,19 +188,21 @@ public class Ejer1 {
 		for (String genero : generos.keySet()) {
 			System.out.println(genero + " " + generos.get(genero));
 		}
+		return generos.size();
 	}
-
+	//Corregido
 	public void peliculasNDirectores(Document doc, int n) {
 		int cont = 0;
 		String titulo = "";
 		NodeList peliculas = doc.getElementsByTagName("pelicula");
 		for (int i = 0; i < peliculas.getLength(); i++) {
+			titulo = "";
 			NodeList datos = peliculas.item(i).getChildNodes();
 			for (int j = 0; j < datos.getLength(); j++) {
-				if (datos.item(j).getNodeName() == "director") {
+				if (datos.item(j).getNodeName().equals("director")) {
 					cont++;
 				}
-				if (datos.item(j).getNodeName() == "titulo") {
+				if (datos.item(j).getNodeName().equals("titulo")) {
 					titulo = datos.item(j).getFirstChild().getNodeValue();
 				}
 			}
@@ -109,8 +212,8 @@ public class Ejer1 {
 			cont = 0;
 		}
 	}
-
-	public void arbolRecursivo(Node doc, NodeList padre, int pro) {
+	//Corregido
+	public void arbolRecursivo(NodeList padre, int pro) {
 		pro++;
 		for (int i = 0; i < padre.getLength(); i++) {
 			for (int j = 0; j < pro; j++) {
@@ -118,18 +221,18 @@ public class Ejer1 {
 			}
 			System.out.println(padre.item(i).getNodeType() + " " + padre.item(i).getNodeName());
 			if (padre.item(i).hasChildNodes()) {
-				arbolRecursivo(doc, padre.item(i).getChildNodes(), pro);
+				arbolRecursivo( padre.item(i).getChildNodes(), pro);
 			}
 		}
 	}
-
+	//Corregido
 	public void getDato(Document doc, String tag) {
 		NodeList titulos = doc.getElementsByTagName(tag);
 		for (int i = 0; i < titulos.getLength(); i++) {
 			System.out.println(tag + ": " + titulos.item(i).getFirstChild().getNodeValue());
 		}
 	}
-
+	//Corregido
 	public void getTituloNombreApellidoGenero(Document doc) {
 		NodeList titulos = doc.getElementsByTagName("titulo");
 		for (int i = 0; i < titulos.getLength(); i++) {
@@ -159,7 +262,7 @@ public class Ejer1 {
 			System.out.println();
 		}
 	}
-
+	//Corregido
 	public Document creaArbol(String ruta) {
 		Document doc = null;
 		try {
