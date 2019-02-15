a518fdea3431c7b1c271ca0fed2b4ea3731ddb5e
Ero Sobrino <ero.sobrino.dorado@gmail.com>
2019-02-15 10:36:49 +0100

    15/2/19

--- src/ejercicios/GestionaDeportistas.java
+++ src/ejercicios/GestionaDeportistas.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -23,7 +24,88 @@ public class GestionaDeportistas {
 	String password = "";
 
 	@GET
-	@Path("/e")
+	@Path("/activos2")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public ArrayList<Deportista> getActivos2() {
+		JDBC database = new JDBC();
+		database.abrirConexion(bd, servidor, usuario, password);
+		try {
+			database.consultarActivos();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		database.cerrarConexion();
+		return deportistas;
+	}
+
+	// 5
+	@GET
+	@Path("/activos")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public ArrayList<Deportista> getActivos() {
+		ArrayList<Deportista> deportistasActivos = new ArrayList<>();
+		JDBC database = new JDBC();
+		database.abrirConexion(bd, servidor, usuario, password);
+		try {
+			database.consultarTodos();
+			for (Deportista deportista : deportistas) {
+				if (deportista.isActivo()) {
+					deportistasActivos.add(deportista);
+				}
+			}
+		} catch (SQLException e) {
+		}
+		database.cerrarConexion();
+		return deportistasActivos;
+	}
+
+	// 4
+	@GET
+	@Path("/deporte/{nombre}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public ArrayList<Deportista> getDeporteNombre(@PathParam("nombre") String deporte) {
+		JDBC database = new JDBC();
+		ArrayList<Deportista> deportistasDeporte = new ArrayList<>();
+		database.abrirConexion(bd, servidor, usuario, password);
+		try {
+			database.consultarTodos();
+			for (Deportista deportista : deportistas) {
+				if (deportista.getDeporte().toUpperCase().equals(deporte.toUpperCase())) {
+					deportistasDeporte.add(deportista);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		database.cerrarConexion();
+		return deportistasDeporte;
+	}
+
+	// 3
+	@GET
+	@Path("/{id}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Deportista getID(@PathParam("id") int id) {
+		JDBC database = new JDBC();
+		Deportista deport = null;
+		database.abrirConexion(bd, servidor, usuario, password);
+		try {
+			database.consultarTodos();
+			for (Deportista deportista : deportistas) {
+				if (deportista.getId() == id) {
+					deport = deportista;
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		database.cerrarConexion();
+		return deport;
+	}
+
+	// 2
+	@GET
+	@Path("/")
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public ArrayList<Deportista> getTodos() {
 		JDBC database = new JDBC();
@@ -57,8 +139,30 @@ public class GestionaDeportistas {
 			}
 		}
 
+		private void consultarActivos() throws SQLException {
+			String query = "SELECT * from deportistas where activo=1;";
+			Statement stmt = conexion.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			deportistas.clear();
+			while (rs.next()) {
+				Deportista deportista = new Deportista();
+				deportista.setId(rs.getInt("id"));
+				deportista.setNombre(rs.getString("nombre"));
+				deportista.setActivo(rs.getBoolean("activo"));
+				deportista.setGenero(rs.getString("genero"));
+				deportista.setDeporte(rs.getString("deporte"));
+				deportistas.add(deportista);
+			}
+		}
+
 		public void abrirConexion(String bd, String servidor, String usuario, String password) {
 			try {
+				try {
+					Class.forName("org.mariadb.jdbc.Driver");
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				String url = String.format("jdbc:mariadb://%s:3306/%s", servidor, bd);
 				this.conexion = DriverManager.getConnection(url, usuario, password); // Establecemos la conexión con la
 																						// BD
